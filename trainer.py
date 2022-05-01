6ed2840fb855ba14df3bf9f0c9f9fc24c6155c02
Víctor Ballester <victor.ballester.ribo@gmail.com>
2022-05-01 16:50:12 +0200

    updated things + gitignore

--- trainer.py
+++ trainer.py
@@ -1,10 +1,11 @@
 import math
 import random
 from typing import TextIO
+import sys
 
 
 class word:
-  def __init__(self, type: str, eng_word: str,
+  def __init__(self, type: str, eng_word: list[str],
                spa_word: list[str], occur: int):
     self.type = type
     self.eng_word = eng_word
@@ -16,24 +17,30 @@ class word:
 
   def setSpaWord(self):
     S: list[str] = []
-    for W in self.spa_word:
-      if W.find('(') != -1:  # if there is a paranthesis
-        S.append(W[:W.index('(') - 1] + W[W.index(')') + 1:])
-      else:  # if there isn't a paranthesis
-        S.append(W)
+    for w in self.spa_word:
+      while w.count('(') > 0:
+        w = w[:w.index('(') - 1] + w[w.index(')') + 1:]
+      S.append(w)
+    # for w in self.spa_word:
+    #   if w.find('(') != -1:  # if there is a paranthesis
+    #     S.append(w[:w.index('(') - 1] + w[w.index(')') + 1:])
+    #   else:  # if there isn't a paranthesis
+    #     S.append(w)
     self.use_spa_word = S
 
+  def setEngWord(self):
+    S: list[str] = []
+    for w in self.eng_word:
+      while w.count('(') > 0:
+        w = w[:w.index('(') - 1] + w[w.index(')') + 1:]
+      S.append(w)
+      # #if W.find('(') != -1:  # if there is a paranthesis
+      #   S.append(W[:W.index('(') - 1] + W[W.index(')') + 1:])
+      # else:  # if there isn't a paranthesis
+      #   S.append(W)
+    self.use_eng_word = S
+
   def wType(self):
-    dict = {
-        'v': "verb",
-        'f': "phrasal verb",
-        'p': "phrase",
-        'n': "noun",
-        'i': "idiom",
-        'a': "adverb",
-        'A': "adjective",
-        'P': "preposition",
-    }
     return dict.get(self.type)
 
 
@@ -43,6 +50,16 @@ file_incor = "incorrect.txt"
 Words: list[word] = []
 Inc: list[int] = []
 sep = ", "
+dict = {
+    'v': "verb",
+    'f': "phrasal verb",
+    'p': "phrase",
+    'n': "noun",
+    'i': "idiom",
+    'a': "adverb",
+    'A': "adjective",
+    'P': "preposition",
+}
 
 
 # transforms the character '1' (for example) into the number 1.
@@ -81,12 +98,14 @@ def read_data(file_words: str, file_occur: str):
   freq, S = 0, sum(L2)
   for line in L1:
     i = line.index('|', 3)  # index of they second '|'.
-    trans = line[i + 2:len(line) - 1]
+    eng = line[4:i - 1]
+    esp = line[i + 2:len(line) - 1]
     # 'len(line) - 1' in order not to read the EOL character '\n'.
-    X = word(line[0], line[4:i - 1], trans.split(sep), L2[len(Words)])
+    X = word(line[0], eng.split(sep), esp.split(sep), L2[len(Words)])
     # we use the 'len(Words)' counter in order not to create a new one
     freq = freq + X.occur / S
     X.setFreq(freq)
+    X.setEngWord()
     X.setSpaWord()
     Words.append(X)
 
@@ -112,65 +131,112 @@ def upgrade_content(file_name: str):  # upgrade the content in "file_occur"
       file.write(str(i.occur) + "\n")
 
 
+def text_format(t: str):
+  if t == 'es':
+    return "English"
+  else:
+    return "Spanish"
+
+
+def choice(n: int, t: str):
+  if t == 'es':
+    return [Words[n].eng_word, Words[n].use_eng_word]
+  else:
+    return [Words[n].spa_word, Words[n].use_spa_word]
+
+
 # performs the comparison between the answered word and the correct word
-def guess_word(n: int, file_occur: str):
-  print(
-      "\nEnglish word:\t    {0} ({1})".format(
-          Words[n].eng_word,
-          Words[n].wType()))
-  guess = input("Guess spanish word: ")
-  if not guess:
-    return -1
-  G = [compare_strings(guess, str) for str in Words[n].use_spa_word]
+def guess_word(n: int, file_occur: str, count: int, t: str):
+  if t == 'b':
+    s = random.choice(['se', 'es'])
+  else:
+    s = t
+  # s[::-1] is to revert the order of the string t. That is: 'es' --> 'se'
+  # and 'se' --> 'es'.
+  r = s[::-1]
+  print("\n{c} word:\t\t         ".format(c=text_format(s)), end="")
+  print(*choice(n, s)[0], sep=', ', end=" ")
+  print("({0})".format(Words[n].wType()))
+  try:
+    if (s == 'se'):
+      first_char = Words[n].eng_word[0][0]
+      nWords = min([i.count(' ') for i in Words[n].use_eng_word]) + 1
+      guess = input(
+          "Guess {c} word ({c1} word/s):   {c2}".format(
+              c=text_format(r).lower(),
+              c1=nWords,
+              c2=first_char))  # .lower() is to lowercase the word
+      guess = first_char + guess
+    else:
+      guess = input(
+          "Guess {c} word:\t         ".format(
+              c=text_format(r).lower()))
+  except KeyboardInterrupt:
+    game_exit(count)
+  G = [compare_strings(guess, str) for str in choice(n, r)[1]]
   if sum(G) > 0:  # check if at least there is one correct answer
     print("Correct!\n")
     if Words[n].occur > 1:
       Words[n].occur -= 1
   else:
-    Inc.append(n)
+    if Inc.count(n) == 0:
+      Inc.append(n)
     print("Incorrect. The correct answers were: ")
-    print(*Words[n].spa_word, sep=', ')
+    print(*choice(n, r)[1], sep=', ')
     Words[n].occur += 1
   upgrade_content(file_occur)
 
 
 def game_exit(count: int):  # game function for finishing the game
-  with open(file_incor, 'w') as file:
-    for i in Inc:
-      file.write(str(i) + "\n")
+  if len(Inc) > 0:
+    with open(file_incor, 'w') as file:
+      for i in Inc:
+        file.write(str(i) + "\n")
   if count != 0:
-    print("\n###############################")
+    print("\n\n###############################")
     print("Correct answers: {c}/{t}\nFrequency of success: {s:.3f}".format(
         c=count - len(Inc), t=count, s=(count - len(Inc)) / count))
     print("###############################\n")
     if len(Inc) > 0:
       print("Summary of incorrect words:\n")
       for i in Inc:
-        print(Words[i].eng_word + ": ", end="")
+        print(*Words[i].eng_word, sep=', ', end=": ")
         print(*Words[i].spa_word, sep=', ')
   print("\nExiting the program.")
+  sys.exit(0)
 
 
 def game(file_occur: str, file_incor: str):  # the game
+  print("Which game do you want to play?\n")
+  print("1- Spanish -> English (type 'se')\n2- English -> Spanish (type 'es')\n3- Both types (type 'b')\n")
+  try:
+    t = input()
+  except KeyboardInterrupt:
+    game_exit(0)
+  if t not in ['se', 'es', 'b']:
+    print("Error entering the data.")
+    game_exit(0)
   count = 0
   with open(file_incor, 'r') as file:
     for i in file.readlines():
       if count == 0:
         print("Review of last day's game:\n")
-      if guess_word(int(i), file_occur) == -1:
-        game_exit(count)
-        exit()
+      guess_word(int(i), file_occur, count, t)
       count += 1
-  print("\n\nWhich modality do you want to play?\n")
-  print("1-Only verbs (type 'v')\n2-Only phrasal verbs (type 'F')\n3-Only phrases (type 'p')\n4-Only nouns (type 'n')\n5-Only idioms (type 'i')\n6-Only adjectives (type 'A')\n7-Only adverbs (type 'a')\n8-Only prepositions (type 'P')\n9-Any category (type '-')\n")
-  c = input()
-  print("Remember: Hit 'Enter' to exit the game whenever you want. Let's play!\n")
-  while 1 > 0:
+  print("Which modality do you want to play?\n")
+  print("1-Only verbs (type 'v')\n2- Only phrasal verbs (type 'F')\n3- Only phrases (type 'p')\n4-Only nouns (type 'n')\n5- Only idioms (type 'i')\n6- Only adjectives (type 'A')\n7-Only adverbs (type 'a')\n8- Only prepositions (type 'P')\n9- Any category (type '-')\n")
+  try:
+    c = input()
+  except KeyboardInterrupt:
+    game_exit(count)
+  if c not in dict.keys() and c != '-':
+    print("Error entering the data.")
+    game_exit(count)
+  print("Remember: 'Ctrl + C' to exit the game whenever you want. Let's play!\n")
+  while True:
     n = cond_random()
     if c == Words[n].type or c == '-':
-      if guess_word(n, file_occur) == -1:
-        game_exit(count)
-        exit()
+      guess_word(n, file_occur, count, t)
       count += 1
 
 
