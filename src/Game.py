b49984598045d7cc589ac121c65d7fa738b6fbd1
Víctor <victor.ballester.ribo@gmail.com>
2023-05-07 17:31:36 +0200

    upgraded version

--- src/Game.py
+++ src/Game.py
@@ -1,35 +1,37 @@
 # sets the language of the game
 import random
-from FileHandling import upgrade_content
+from FileHandling import *
 from misc import *
 
 
 def pre_game():
     print("Welcome to the Vocabulary Trainer!")
-    print("This program will help you to learn new words.")
-    print("Which language do you want to learn?")
+    print("This program will help you to learn new words.\n")
+    print("Which language do you want to learn?\n")
     print("1. English")
-    print("2. French")
+    print("2. French\n")
     while (True):
+        i = 0
         try:
             i = int(input("Select a number: "))
             if i != 1 and i != 2:
                 print("Please, enter a valid number.")
                 continue
         except KeyboardInterrupt:
-            game_exit(0, "")
-            return
+            game_exit(0, "", [], [])
         except ValueError:
             print("Please, enter a number.".format())
             continue
+        lang = "NOT DEFINED"
         if i == 1:
-            return "en", "data/en/vocab.txt", "data/en/occurrences.txt", "data/en/incorrect.txt"
-        else:  # i == 2:
-            return "fr", "data/fr/vocab.txt", "data/fr/occurrences.txt", "data/fr/incorrect.txt"
+            lang = "en"
+        elif i == 2:
+            lang = "fr"
+        return lang
 
 
 # performs the comparison between the answered word and the correct word
-def guess_word(n: int, file_occur: str, file_incor: str, count: int, t: str):
+def guess_word(n: int, lang: str, count: int, t: str, Words: list[word], Inc: list[int]):
     if t == "b":
         # sf = spanish to foreign, fs = foreign to spanish
         s = random.choice(["sf", "fs"])
@@ -38,29 +40,35 @@ def guess_word(n: int, file_occur: str, file_incor: str, count: int, t: str):
     # s[::-1] revert the order of the string t. That is: 'fs' --> 'sf' and
     # 'sf' --> 'fs'.
     r = s[::-1]
-    print("\n{c} word:\t\t\t".format(c=text_format(s)), end="")
-    print(*choice(n, s)[0], sep=", ", end=" ")
+    print("\n{c} word:\t\t\t".format(c=text_format(s, lang)), end="")
+    print(*choice(n, s, Words)[0], sep=", ", end=" ")
     print("({0})".format(Words[n].wType()))
     try:
         if s == "sf":
-            first_char = Words[n].foreign_word[0][0]
             nWords = min([i.count(" ") for i in Words[n].use_foreign_word]) + 1
+            j = 0
+            while (Words[n].foreign_word[j].count(" ") > nWords - 1):
+                j += 1
+            first_char = Words[n].foreign_word[j][0]
         else:
-            first_char = Words[n].spa_word[0][0]
             nWords = min([i.count(" ") for i in Words[n].use_spa_word]) + 1
+            j = 0
+            while (Words[n].spa_word[j].count(" ") > nWords - 1):
+                j += 1
+            first_char = Words[n].spa_word[j][0]
         guess = input(
             "Guess {c} word ({c1} word/s):\t{c2}".format(
-                c=text_format(r).lower(), c1=nWords, c2=first_char
+                c=text_format(r, lang).lower(), c1=nWords, c2=first_char
             )
         )  # .lower() is to lowercase the word
         Guess = [first_char + guess, guess]
     except KeyboardInterrupt:
-        game_exit(count, file_incor)
+        game_exit(count, lang, Words, Inc)
         return
     G = [
         compare_strings(Guess[0], str) +
         compare_strings(Guess[1], str)
-        for str in choice(n, r)[1]
+        for str in choice(n, r, Words)[1]
     ]
     if sum(G) > 0:  # check if at least there is one correct answer
         print(f"{bcolors.OKGREEN}Correct!\n{bcolors.ENDC}")
@@ -69,17 +77,17 @@ def guess_word(n: int, file_occur: str, file_incor: str, count: int, t: str):
         if Inc.count(n) == 0:
             Inc.append(n)
         print(f"{bcolors.FAIL}Incorrect. The correct answers were: {bcolors.ENDC}")
-        print(*choice(n, r)[1], sep=", ")
+        print(*choice(n, r, Words)[1], sep=", ")
         Words[n].occur += 1
-    upgrade_content(file_occur)
+    upgrade_content(lang, Words)
 
 # the game
 
 
-def game(file_occur: str, file_incor: str):
+def game(lang: str, Words: list[word], Inc: list[int]):
     print("Which game do you want to play?\n")
     print(
-        "1. Spanish -> {c}\n2. {c} -> Spanish\n3. Both types\n".format(c=language()))
+        "1. Spanish -> {c}\n2. {c} -> Spanish\n3. Both types\n".format(c=language(lang)))
 
     while (True):
         try:
@@ -88,7 +96,7 @@ def game(file_occur: str, file_incor: str):
                 print("Please, enter a valid number.")
                 continue
         except KeyboardInterrupt:
-            game_exit(0, file_incor)
+            game_exit(0, lang, Words, Inc)
             return
         except ValueError:
             print("Please, enter a number.".format())
@@ -101,11 +109,11 @@ def game(file_occur: str, file_incor: str):
             t = "b"
         break
     count = 0
-    with open(file_incor, "r") as file:
+    with open(getFileIncor(lang), "r") as file:
         for i in file.readlines():
             if count == 0:
                 print("Review of last day's game:\n")
-            guess_word(int(i), file_occur, file_incor, count, t)
+            guess_word(int(i), lang, count, t, Words, Inc)
             count += 1
             if count == len(file.readline()):
                 print("\n")
@@ -120,7 +128,7 @@ def game(file_occur: str, file_incor: str):
                 print("Please, enter a valid number.")
                 continue
         except KeyboardInterrupt:
-            game_exit(0, file_incor)
+            game_exit(0, lang, Words, Inc)
             return
         except ValueError:
             print("Please, enter a number.".format())
@@ -145,12 +153,12 @@ def game(file_occur: str, file_incor: str):
     print("Remember: 'Ctrl + C' to exit the game whenever you want. Let's play!\n")
     count_rep = 0
     while count_rep < 10*len(Words):
-        n = cond_random()
+        n = cond_random(Words)
         if c == Words[n].type or c == "-":
-            guess_word(n, file_occur, file_incor, count, t)
+            guess_word(n, lang, count, t, Words, Inc)
             count += 1
             count_rep = 0
         else:
             count_rep += 1
     print("There are no words of the type '{0}'.".format(c))
-    game_exit(count, file_incor)
+    game_exit(count, lang, Words, Inc)
